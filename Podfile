6a6e3b89d61391a4b2e25dbfb69a2640a07bb5f1
Ashni Sheth <22asheth@castilleja.org>
2022-04-07 08:48:04 -0700

    Model added, connection to Firebase successful, in process of testing for getting the model connected to images

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SpotFirebase' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+pod 'Firebase/Core'
+pod 'Firebase/Storage'
+
+  target 'SpotFirebaseTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SpotFirebaseUITests' do
+    # Pods for testing
+  end
+
+end
--- SpotFirebase/ViewController.swift
+++ SpotFirebase/ViewController.swift
@@ -6,14 +6,172 @@
 //
 
 import UIKit
+import FirebaseStorage
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var firstRun = true
+    var photoUploadFail = false
+    
+    /// A predictor instance that uses Vision and Core ML to generate prediction strings from a photo.
+    let imagePredictor = ImagePredictor()
 
+    /// The largest number of predictions the main view controller displays the user.
+    let predictionsToShow = 1
+    var imageToPredict = UIImage()
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var label: UILabel!
+    
+    private let storage = Storage.storage().reference()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        imageView.contentMode = .scaleAspectFit
         // Do any additional setup after loading the view.
+        
+//        guard let urlString = UserDefaults.standard.value(forKey: "url") as? String,
+//        let url = URL(string: urlString) else {
+//            return
+//        }
+        
+        //label.text = urlString
+        
+//        let task = URLSession.shared.dataTask(with: url, completionHandler: { data, _, error in
+//            guard let data = data, error == nil else {
+//                return
+//            }
+//
+//            DispatchQueue.main.async {
+//                let image = UIImage(data: data)
+//                self.imageView.image = image
+//            }
+//        })
+        
+        //task.resume()
+    }
+    
+    @IBAction func didTapButton() {
+        
+        //get download url
+        let storage = Storage.storage()
+        let storageRef = storage.reference()
+        let imageRef = storageRef.child("pictures/image.jpg")
+        
+        imageRef.downloadURL(completion: { [self] url, error in
+            guard let url = url, error == nil else {
+                return
+            }
+            
+            let urlString = url.absoluteString
+            
+            if let data = try? Data(contentsOf: url) {
+                self.imageView.image = UIImage(data: data)
+                self.imageToPredict = self.imageView.image!
+                self.label.text = urlString
+            }
+            
+//            DispatchQueue.main.async {
+//                self.label.text = urlString
+//                self.imageView.image = image
+//            }
+//        let picker = UIImagePickerController()
+//        picker.sourceType = .photoLibrary
+//        picker.delegate = self
+//        picker.allowsEditing = true
+//        present(picker, animated: true)
+    })
+        
+    }
+}
+
+
+extension ViewController {
+    // MARK: Main storyboard updates
+    /// Updates the storyboard's image view.
+    /// - Parameter image: An image.
+    func updateImage(_ image: UIImage) {
+        DispatchQueue.main.async {
+            self.imageView.image = image
+        }
     }
 
+    /// Updates the storyboard's prediction label.
+    /// - Parameter message: A prediction or message string.
+    /// - Tag: updatePredictionLabel
+    func updatePredictionLabel(_ message: String) {
+        DispatchQueue.main.async {
+            //self.predictionLabel.text = message -- put in where we want to display this info
+        }
+
+        if firstRun {
+            DispatchQueue.main.async {
+                self.firstRun = false
+                //self.predictionLabel.superview?.isHidden = false
+                //self.startupPrompts.isHidden = true
+            }
+        }
+    }
+    /// Notifies the view controller when a user selects a photo in the camera picker or photo library picker.
+    /// - Parameter photo: A photo from the camera or photo library.
+    func userSelectedPhoto(_ photo: UIImage) {
+        updateImage(photo)
+        updatePredictionLabel("Making predictions for the photo...")
+
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.classifyImage(photo)
+        }
+    }
 
 }
 
+extension ViewController {
+    // MARK: Image prediction methods
+    /// Sends a photo to the Image Predictor to get a prediction of its content.
+    /// - Parameter image: A photo.
+    private func classifyImage(_ image: UIImage) {
+        do {
+            try self.imagePredictor.makePredictions(for: image,
+                                                    completionHandler: imagePredictionHandler)
+            
+        } catch {
+            print("Vision was unable to make a prediction...\n\n\(error.localizedDescription)")
+        }
+    }
+
+    /// The method the Image Predictor calls when its image classifier model generates a prediction.
+    /// - Parameter predictions: An array of predictions.
+    /// - Tag: imagePredictionHandler
+    private func imagePredictionHandler(_ predictions: [ImagePredictor.Prediction]?) {
+        guard let predictions = predictions else {
+            updatePredictionLabel("No predictions. (Check console log.)")
+            return
+        }
+
+        let formattedPredictions = formatPredictions(predictions)
+
+        let predictionString = formattedPredictions.joined(separator: "\n")
+        updatePredictionLabel(predictionString)
+    }
+
+    /// Converts a prediction's observations into human-readable strings.
+    /// - Parameter observations: The classification observations from a Vision request.
+    /// - Tag: formatPredictions
+    private func formatPredictions(_ predictions: [ImagePredictor.Prediction]) -> [String] {
+        // Vision sorts the classifications in descending confidence order.
+        let topPredictions: [String] = predictions.prefix(predictionsToShow).map { prediction in
+            var name = prediction.classification
+
+            // For classifications with more than one name, keep the one before the first comma.
+            if let firstComma = name.firstIndex(of: ",") {
+                name = String(name.prefix(upTo: firstComma))
+            }
+
+            return "\(name) - \(prediction.confidencePercentage)%"
+        }
+
+        return topPredictions
+    }
+}
